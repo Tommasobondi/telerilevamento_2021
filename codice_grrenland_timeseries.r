08a71d6e63d1fbea9d44fd9ae2c82083da81295e
Tommasobondi <80787909+Tommasobondi@users.noreply.github.com>
2021-04-07 11:46:43 +0200

    Update codice_grrenland_timeseries.r

--- codice_grrenland_timeseries.r
+++ codice_grrenland_timeseries.r
@@ -2,3 +2,7 @@
 #same base functin of codice commentato in mine github
 library(raster)
 setwd("C:/lab/greenland")
+#now we charge the datatset in R with function raster(diverso da codice precente, dove caricavo con brick), the i plot it to see
+duemila<-raster("lst_2000.tif")
+plot(duemila)
+

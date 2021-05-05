7f7a02e6e0fc3da8aaf527be4f59a90c31f1c58e
Tommasobondi <80787909+Tommasobondi@users.noreply.github.com>
2021-05-05 12:29:32 +0200

    Update land_cover.r

--- land_cover.r
+++ land_cover.r
@@ -1,3 +1,11 @@
 library(raster)
 library(RStoolbox)
+library(ggplot2)
 setwd("C:/lab/vege")
+#https://ggplot2-book.org/
+#now we need a dataset, but is a multilevel dataset, so func. brick
+d1<-brick("defor1.jpg")
+#visualize info and plot
+d1
+plotRGB(d1, r=1, g=2, b=3, stretch="lin")
+

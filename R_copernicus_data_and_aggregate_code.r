f6ad78180ae8f5efe48eaedae96f5a2c807d515f
Tommasobondi <80787909+Tommasobondi@users.noreply.github.com>
2021-05-21 12:34:55 +0200

    Update and rename copernicus_data to R_copernicus_data_and_aggregate_code.r

--- R_copernicus_data_and_aggregate_code.r
+++ R_copernicus_data_and_aggregate_code.r
@@ -1,6 +1,8 @@
 #how to visualize copoernicus data
+#copernicus data shortcut in desktop
 library(rasterVis)
 #ncdf4 useful to visualize .nc file format data
+#https://cran.r-project.org/web/packages/ncdf4/ncdf4.pdf
 library(ncdf4)
 setwd("c:/lab/copernicus/")
 #setted all the preliminary stuff, charge and visualize the dataset(use raster because i have only one layer)
@@ -10,6 +12,7 @@ cl <- colorRampPalette(c('blue','light blue','green','red','yellow'))(100)
 plot(cry,col=cl)
 #ricampiono variabile originale, la rendo piu leggera
 #applico ricampionamento linerae, per ogni X pixel origianli creo 1 pixel nuovo con palore pari a media pixel originale
-#funz è aggregate, argomento oggetto da ricampionare e fact8 fattore di riduzione)
+#funz è aggregate, argomento oggetto da ricampionare e fact fattore di riduzione)
+#https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/aggregate
 cryricamp <- aggregate(cry, fact=100)
 plot(cryricamp, col=cl)
